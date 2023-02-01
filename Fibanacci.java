f6f323e522dd7a89cd7195e961c942abbb1cf2b1
Chandannbongale <121491453+Chandannbongale@users.noreply.github.com>
2023-02-01 22:44:25 +0530

    Add files via upload

---
+++ Fibanacci.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Fibanacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=sc.nextInt();
+		int n1=0,n2=1,n3 = 0;
+		int sum=0;
+		System.out.print(n1+" "+n2);
+		for(int i=2;i<=n;i++) 
+		{
+			//sum=sum+(n3);
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			n1=n2;
+			n2=n3;
+			sum+=n3;
+			}
+			System.out.println("\n the sum is="+sum);
+		}
+				
+
+	}
+
+
